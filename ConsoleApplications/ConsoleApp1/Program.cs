2274b3c4bf9fc4751199a1d1f2692ecd0e27d8d4
sejal <sejal-panchal@users.noreply.github.com>
2021-06-06 22:14:43 -0600

    ConsoleApps

---
+++ ConsoleApplications/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the sockMerchant function below.
+    //refer:https://medium.com/@rezehnde/sock-merchant-challenge-solved-in-c-f2ec9235ddea
+    ///For example, there are n = 7 socks with colors ar = [1, 2, 1, 2, 1, 3, 2].
+    ///There is one pair of color 1 and one of color 2.
+    ///There are three odd socks left, one of each color. 
+    ///The number of pairs is 2.
+    static int sockMerchant(int n, int[] ar)
+    {
+       
+        int counter = 0;
+        var comparlist = new Dictionary<int,int>();
+        foreach (var i in ar) 
+        {           
+            if (comparlist.ContainsKey(i))
+            {
+                counter++;
+                comparlist.Remove(i);
+            }
+            else {
+                comparlist.Add(i,1);
+            }          
+           
+        }
+        
+       return counter;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int n = ar.Length;
+        int result = sockMerchant(n, ar);
+
+        Console.WriteLine("paired socks: " + result);
+
+    }
+}
---
+++ ConsoleApplications/CountGroup/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountGroup
+{
+    class Program
+    {
+        //count group formed in matrices by binaries (0 and 1) such that 1 represents relation and 0 does not. Example data below:
+        //1100
+        //1100
+        //0010
+        //0001
+        //related[n]
+        //related[0]
+        //related[1]
+        //related[2]
+
+        //equals one group
+
+        //and related[3] = not related to anybody else so it forms its own group
+
+        //so total group here are 2 groups
+
+        public static int Count(Char[][] related)
+        {
+            int noOfGroup = 0;
+            Boolean[] visited = new bool[related.Length];
+            for (int i = 0; i < visited.Length; i++)
+                visited[i] = false;
+
+            for (int i = 0; i < related.Length; i++)
+            {
+                if (!visited[i])
+                {
+                    noOfGroup++;
+                    visited[i] = true;
+                    findRelation(related, visited, i);
+                }
+            }
+
+            return noOfGroup;
+        }
+
+        public static void findRelation(Char[][] relation, Boolean[] visited, int p)
+        {
+            for (int q = 0; q < relation.Length; q++)
+            {
+                if (!visited[q] && p != q && relation[p][q] == '1')
+                {
+                    visited[q] = true; //relations are found here in form of node visited
+                    findRelation(relation, visited, q);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> related = new List<string> { "1100", "1110", "0110", "0001" };
+            //Char[,] bins= new Char[4,4]
+            //{ {'1','1','0','0'},
+            // {'1','1','1','0' },
+            // {'0','1','1','0' },
+            // { '0','0','0','1'}
+            //};
+
+            // create array of arrays
+            char[][] possibleGroup = new char[related.Count][];
+            // create arrays to put in the array of arrays
+            for (int i = 0; i < related.Count; i++)
+            {
+                possibleGroup[i] = new char[related.Count];
+                // put array in array of arrays
+                possibleGroup[i] = related[i].ToCharArray();//row as array
+            }
+            Console.WriteLine("No of groups are: " + Count(possibleGroup));
+        }
+    }
+}
