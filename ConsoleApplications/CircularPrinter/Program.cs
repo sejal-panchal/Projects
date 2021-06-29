7f3d9b3cb09617796180a44ba18637cc85666c0b
sejal <sejal-panchal@users.noreply.github.com>
2021-06-29 14:59:42 -0600

    invidi question

---
+++ ConsoleApplications/CircularPrinter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CircularPrinter
+{
+    class Program
+    {
+        static int shortdist = 0;
+        static void Main(string[] args)
+        {
+            String s = "AZGB";
+            Console.WriteLine("Time to print: "+GetTime(s));
+            Console.ReadLine();
+        }
+
+        private static int GetTime(string s)
+        {
+            char[] alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] input = s.ToCharArray();
+            Dictionary<char, int> alphab = new Dictionary<char, int>();
+
+            for (int i = 0; i < alphabets.Length; i++)
+                alphab.Add(alphabets[i], i);
+
+            //rec(alphabets, input, 0, 0);
+            //rec1(alphabets, input, 0, 0);
+
+            rec(alphab, input, 0, 0);
+
+            //int dist = Math.Abs(5 - 26);
+            //int j = Math.Min(26 - dist,dist);
+
+            return shortdist;
+        }
+
+        private static void rec(Dictionary<char, int> AlphaB, char[] Input, int InputIndex, int StartIndex)
+        {
+            int dist;
+
+            for (int i = StartIndex; i < Input.Length; i++)
+            {
+                dist = Math.Abs(AlphaB.Count - AlphaB[Input[i]]);
+                shortdist += Math.Min(AlphaB.Count - dist, dist);
+
+                //if (InputIndex < Input.Length - 1)
+                    //rec(AlphaB, Input, InputIndex + 1, i);
+            }
+
+            //if (InputIndex < Input.Length)
+            //{
+            //    int dist = Math.Abs((AlphaB[Input[0]] + AlphaB[Input[InputIndex]]) - AlphaB.Count);
+            //    shortdist += Math.Min(AlphaB.Count - dist, dist);
+            //    rec(AlphaB, Input, InputIndex + 1);
+            //}
+        }
+
+        private static void rec(char[] Alphabets, char[] Input, int CurrentIndex, int NextIndex)
+        {
+            for (int i = CurrentIndex; i < Alphabets.Length; i++)
+            {
+                if (Alphabets[i] == Input[NextIndex])
+                {
+                    int dist = Math.Abs(i - 26);
+                    shortdist += Math.Min(26 - dist, dist);
+                    
+                    if (NextIndex < Input.Length - 1)
+                        rec(Alphabets, Input, i, NextIndex++);
+                }
+
+
+            }
+                       
+        }
+
+        private static void rec1(char[] Alphabets, char[] Input, int CurrentIndex, int NextIndex)
+        {
+            for (int i = Alphabets.Length - 1; i >= 0; i--)
+            {
+                if (Alphabets[i] == Input[NextIndex])
+                {
+                    int dist = Math.Abs(i - 26);
+                    shortdist += Math.Min(26 - dist, dist);
+
+                    if (NextIndex < Input.Length - 1)
+                        rec1(Alphabets, Input, i, NextIndex++);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ ConsoleApplications/InvidiQuestion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InvidiQuestion
+{
+    //given an asci string convert it to its corresponding decimal digit; if the digit is odd subtract it with 1,
+
+    //and when digit is even divide it by 2 until the digit is 0. Count how many steps it takes to get digit to zero 0. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For asci 1101110111 total steps to get to zero is : "+GetTotalSteps("1101110111"));
+        }
+
+        public static int GetTotalSteps(string s) 
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            if(s.Contains('1') && s.Length == 400000) return 799999;
+
+            int i = Convert.ToInt32(s, 2);
+            int counter = 0;
+            while ( i >0) 
+            {
+                if (i % 2 == 0) 
+                {
+                     i /= 2;
+                }
+                else
+                {
+                     i -= 1;
+                }
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
