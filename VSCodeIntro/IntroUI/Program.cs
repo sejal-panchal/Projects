1f61548f9734f8d893c827eeec2f956a7cd45c4b
Sejal Panchal <sejalpanchal@Sejals-MacBook-Air.local>
2021-07-07 13:36:57 -0600

    sample project using vscode

---
+++ VSCodeIntro/IntroUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel{ FirstName="sejal",LastName="panchal",Age=36};
+            System.Console.WriteLine($"{p.FirstName} {p.LastName} her age is {p.Age}.");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test ");
+        }
+    }
+}
